7330550438dd34346d009bd280118f15e5c79ebb
Purushottam Dev <pdev@oucs1427.otago.ac.nz>
2013-09-26 14:58:09 +1200

    Initial Commit

---
+++ PdfReader/ReaderDocument.h
@@ -0,0 +1,13 @@
+//
+//  ReaderDocument.h
+//  PdfReader
+//
+//  Created by Purushottam Dev on 9/26/13.
+//  Copyright (c) 2013 Purushottam Dev. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ReaderDocument : NSDocument
+
+@end
