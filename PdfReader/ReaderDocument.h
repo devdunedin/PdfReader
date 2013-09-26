860bb4fd45bae480b58782c321a67565b200f98b
Purushottam Dev <pdev@oucs1395.otago.ac.nz>
2013-09-26 17:17:58 +1200

    save files now

--- PdfReader/ReaderDocument.h
+++ PdfReader/ReaderDocument.h
@@ -7,7 +7,27 @@
 //
 
 #import <Cocoa/Cocoa.h>
+#import <Quartz/Quartz.h>
 
 @interface ReaderDocument : NSDocument
+
+
+//@property (assign) IBOutlet NSWindow *window;
+@property (weak) IBOutlet PDFView *ReaderFrontPage;
+
+@property (weak) IBOutlet PDFThumbnailView *PdfThumbnail;
+
+@property (weak) IBOutlet NSComboBox *PageCounter;
+@property (weak) IBOutlet NSComboBoxCell *currentPageNumber;
+@property (weak) IBOutlet NSComboBoxCell *viewType;
+
+- (IBAction)goBack:(id)sender;
+- (IBAction)goFront:(id)sender;
+- (IBAction)zoomIn:(id)sender;
+
+- (IBAction)zoomOut:(id)sender;
+
+- (IBAction)zoomToFit:(id)sender;
+- (IBAction)kindOfView:(id)sender;
 
 @end
--- PdfReader/ReaderDocument.m
+++ PdfReader/ReaderDocument.m
@@ -7,8 +7,10 @@
 //
 
 #import "ReaderDocument.h"
+#import <Quartz/Quartz.h>
 
 @implementation ReaderDocument
+ PDFDocument *pdfDoc;
 
 - (id)init
 {
@@ -30,7 +32,64 @@
 {
     [super windowControllerDidLoadNib:aController];
     // Add any code here that needs to be executed once the windowController has loaded the document's window.
+   
+    
+    [_ReaderFrontPage setDocument:pdfDoc];
+    [_PdfThumbnail setPDFView:_ReaderFrontPage];
+  
 }
+
+- (IBAction)goBack:(id)sender {
+    if([_ReaderFrontPage canGoToPreviousPage]){
+        [_ReaderFrontPage goToPreviousPage:sender];
+    }
+    
+}
+
+- (IBAction)goFront:(id)sender {
+    if([_ReaderFrontPage canGoToNextPage]){
+        [_ReaderFrontPage goToNextPage:sender];
+    }
+}
+
+- (IBAction)zoomOut:(id)sender {
+    
+    [_ReaderFrontPage zoomOut:sender];
+
+}
+
+- (IBAction)zoomToFit:(id)sender {
+    [_ReaderFrontPage setAutoScales:YES];
+}
+
+- (IBAction)kindOfView:(id)sender {
+    if([_viewType indexOfSelectedItem] == 0){
+        [_ReaderFrontPage setDisplayMode:kPDFDisplaySinglePage];
+    }
+//    else if([_viewType indexOfSelectedItem] == 1){
+//        [_ReaderFrontPage setDisplayMode:kPDFDisplaySinglePageContinuous];
+//    }
+//    else if([_viewType indexOfSelectedItem] == 2){
+//        [_ReaderFrontPage setDisplayMode:kPDFDisplayTwoUp];
+//    }
+//    else if([_viewType indexOfSelectedItem] == 3){
+//        [_ReaderFrontPage setDisplayMode:kPDFDisplayTwoUpContinuous];
+//    }
+
+    }
+
+
+
+- (IBAction)zoomIn:(id)sender {
+    
+    [_ReaderFrontPage zoomIn:sender];
+}
+
+
+
+
+
+
 
 + (BOOL)autosavesInPlace
 {
@@ -46,14 +105,24 @@
     return nil;
 }
 
+- (void)canCloseDocumentWithDelegate:(id)delegate shouldCloseSelector:(SEL)shouldCloseSelector contextInfo:(void *)contextInfo {
+    pdfDoc = nil;
+    [super canCloseDocumentWithDelegate:delegate shouldCloseSelector:shouldCloseSelector contextInfo:contextInfo];
+}
+
 - (BOOL)readFromData:(NSData *)data ofType:(NSString *)typeName error:(NSError **)outError
 {
     // Insert code here to read your document from the given data of the specified type. If outError != NULL, ensure that you create and set an appropriate error when returning NO.
     // You can also choose to override -readFromFileWrapper:ofType:error: or -readFromURL:ofType:error: instead.
     // If you override either of these, you should also override -isEntireFileLoaded to return NO if the contents are lazily loaded.
-    NSException *exception = [NSException exceptionWithName:@"UnimplementedMethod" reason:[NSString stringWithFormat:@"%@ is unimplemented", NSStringFromSelector(_cmd)] userInfo:nil];
-    @throw exception;
+   //OPen initWithData: data
+    pdfDoc = [[PDFDocument alloc] initWithData: data];
+    
     return YES;
 }
 
+
+
 @end
+
+
